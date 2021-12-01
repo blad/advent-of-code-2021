958bbc402cad772e9cb37cbcc59ae3d282277067
Bladymir Tellez <btellez@gmail.com>
2021-12-01 12:57:37 -0800

    Clean Up

--- problems/Day_01/app/src/app/core.clj
+++ problems/Day_01/app/src/app/core.clj
@@ -2,21 +2,17 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(defn zip [xs ys] 
-  (map vector xs ys))
-
-(defn increasing? 
-  "Determines if a value is increasing"
-  [current-value next-value] 
-  (> next-value current-value))
-
-(defn solve-part-1 [values]
+(defn solve-part-1 
+  "Solve Part 1 of Day 1" 
+  [values]
   (let [offset-values   (drop 1 values)
-        collections     (zip values offset-values)
+        collections     (map vector values offset-values)
         count-increases (apply + (map #(if (< (first %) (second %)) 1 0) collections))]
     count-increases))
 
-(defn solve-part-2 [values]
+(defn solve-part-2 
+  "Solve Part 2 of Day 1" 
+  [values]
   (let [values-position-2 (drop 1 values)
         values-position-3 (drop 1 values-position-2)
         collections     (map vector values values-position-2 values-position-3)
