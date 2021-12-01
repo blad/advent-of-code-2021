88227a3d77e8d53f3c9dec183cbaeb742d121792
Bladymir Tellez <btellez@gmail.com>
2021-12-01 12:55:42 -0800

    Solve Day 1 Part II

--- problems/Day_01/app/src/app/core.clj
+++ problems/Day_01/app/src/app/core.clj
@@ -16,8 +16,16 @@
         count-increases (apply + (map #(if (< (first %) (second %)) 1 0) collections))]
     count-increases))
 
+(defn solve-part-2 [values]
+  (let [values-position-2 (drop 1 values)
+        values-position-3 (drop 1 values-position-2)
+        collections     (map vector values values-position-2 values-position-3)
+        sums            (map #(apply + %) collections)]
+    (solve-part-1 sums)))
+
 (defn -main []
   (let [input-file    "resources/input.txt"
         file-contents (slurp input-file)
         inputs        (map #(Integer/parseInt %) (str/split file-contents #"\n"))]
-    (println (solve-part-1 inputs))))
+    ;;; Update the invoked function to toggle between solving part 1 and part 2
+    (println (solve-part-2 inputs))))
