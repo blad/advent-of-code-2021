a34aa3d906753324aa3617cb7f7adbfae5bec2a9
Bladymir Tellez <btellez@gmail.com>
2021-12-02 09:24:59 -0800

    Implement Day 2 Part 2

--- problems/Day_02/src/app/core.clj
+++ problems/Day_02/src/app/core.clj
@@ -23,20 +23,44 @@
    (let [current-value (get state direction 0)]
     (assoc state direction (+ current-value delta))))
  
-(defn up [state delta] 
+;; Functions for updating state w.r.t. part 1
+(defn up-part-1 [state delta] 
   (update-state state :vertical (- delta)))
 
-(defn down [state delta] 
+(defn down-part-1 [state delta] 
   (update-state state :vertical delta))
 
-(defn forward [state delta] 
+(defn forward-part-1 [state delta] 
   (update-state state :horizontal delta))
 
-(defn tally [state [direction value]] 
+(defn tally-part-1 [state [direction value]] 
   (cond 
-    (= :up direction) (up state value)
-    (= :down direction) (down state value)
-    (= :forward direction) (forward state value)
+    (= :up direction) (up-part-1 state value)
+    (= :down direction) (down-part-1 state value)
+    (= :forward direction) (forward-part-1 state value)
+    :else state))
+
+;; Functions for updating state w.r.t. part 2
+(defn up-part-2 [state delta] 
+  (update-state state :aim (- delta)))
+
+(defn down-part-2 [state delta] 
+  (update-state state :aim delta))
+
+(defn compute-depth-delta [{aim :aim} delta] 
+  (* aim delta))
+
+(defn forward-part-2 [state delta] 
+  (let [depth-delta (compute-depth-delta state delta)]
+    (-> state
+      (update-state :horizontal delta)
+      (update-state :vertical depth-delta))))
+
+(defn tally-part-2 [state [direction value]] 
+  (cond 
+    (= :up direction) (up-part-2 state value)
+    (= :down direction) (down-part-2 state value)
+    (= :forward direction) (forward-part-2 state value)
     :else state))
 
 ;; Solver Functions
@@ -47,10 +71,16 @@
   "Solve Part 1 of Day 2" 
   [values]
   (compute-output-value
-    (reduce tally {:horizontal 0 :vertical 0} values)))
+    (reduce tally-part-1 {:horizontal 0 :vertical 0} values)))
+
+(defn solve-part-2
+  "Solve Part 2 of Day 2" 
+  [values]
+  (compute-output-value
+    (reduce tally-part-2 {:horizontal 0 :vertical 0 :aim 0} values)))
 
 (defn -main []
   (let [input-file    "resources/input.txt"
         file-contents (slurp input-file)
         inputs        (convert-input file-contents)]
-    (println (solve-part-1 inputs))))
+    (println (solve-part-2 inputs))))
